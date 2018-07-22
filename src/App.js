4d3110eb612e32d2a91e2b8613c6c68b1194ee87
imariman <imariman@users.noreply.github.com>
2018-07-23 00:33:16 +0300

    bwm react first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {Header} from './shared/Header';
+import { RentalList } from "./components/rental/RentalList";
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Header/>
+                <div className='container'>
+                <RentalList/>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
